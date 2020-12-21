ba18593b867e7fa3592e9e7e68f77f1d5586b7a0
ShaunH1512 <24032620@edgehill.ac.uk>
2020-12-21 17:41:17 +0000

    Board now implemented and appears as required

--- app/components/connect4.js
+++ app/components/connect4.js
@@ -1,9 +1,39 @@
-import Component from '@glimmer/component';
+import Component from "@ember/component";
 
 export default Ember.Component.extend ({
-    actions: {
-        start: function() {
-            alert('Button working correctly');
-        }
+
+    didInsertElement: function() {
+        let stage = new createjs.Stage(this.element.querySelector("#stage"));
+
+        // Draw the game board - color blue fill like a connect 4 board
+        let board = new createjs.Shape();
+        let graphics = board.graphics;
+        graphics.beginFill('#0000ff');
+
+        // Draw the Outside Lines
+        graphics.drawRect(0, 0, 340, 2);
+        graphics.drawRect(340, 0, 2, 300);
+        graphics.drawRect(0, 0, 2, 300);
+        graphics.drawRect(0, 300, 340, 2);
+
+        //Draw Vertical Lines
+        graphics.drawRect(48.5, 0, 2, 300);
+        graphics.drawRect(97, 0, 2, 300);
+        graphics.drawRect(145.5, 0, 2, 300);
+        graphics.drawRect(194, 0, 2, 300);
+        graphics.drawRect(242.5, 0, 2, 300);
+        graphics.drawRect(291, 0, 2, 300);
+
+        //Draw Horizontal Lines
+        graphics.drawRect(0, 50, 340, 2);
+        graphics.drawRect(0, 100, 340, 2);
+        graphics.drawRect(0, 150, 340, 2);
+        graphics.drawRect(0, 200, 340, 2);
+        graphics.drawRect(0, 250, 340, 2);
+
+        board.x = 20;
+        board.y = 40;
+        stage.addChild(board);
+        stage.update()
     }
 });
