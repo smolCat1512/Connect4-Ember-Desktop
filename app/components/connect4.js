824bb45ac89bd1e8854a94889f78508ae8d3fc92
ShaunH1512 <24032620@edgehill.ac.uk>
2020-12-22 11:25:16 +0000

    Created markers variable with the x and o array variables. Also for loop for the pieces created, and the required yellow and red piece code completed. Sizes will need amending but at this stage all on track and working

--- app/components/connect4.js
+++ app/components/connect4.js
@@ -2,6 +2,8 @@ import Component from "@ember/component";
 
 export default Ember.Component.extend ({
 
+    // Creation of didInsertElement function, this encompasses all
+    // the required game functions
     didInsertElement: function() {
         let stage = new createjs.Stage(this.element.querySelector("#stage"));
 
@@ -31,9 +33,53 @@ export default Ember.Component.extend ({
         graphics.drawRect(0, 200, 340, 2);
         graphics.drawRect(0, 250, 340, 2);
 
+        // start drawing from x and y board co-ordinates, then add
+        // board to the stage variable
         board.x = 20;
         board.y = 40;
         stage.addChild(board);
+
+        
+        // Create players markers/pieces, leave as x and o as easier
+        //than setting long names
+        let markers = {
+            'x': [],
+            'o': []
+        }
+        // for loop, 21 counters per player as there are 42 places
+        // on the connect 4 board
+        for(let x = 0; x < 21; x++) {
+            //Create the yellow token/game piece
+            let yellowPiece = new createjs.Shape();
+            graphics = yellowPiece.graphics;
+            graphics.beginStroke('#FFFF00');
+            graphics.beginFill('#FFFF00');
+            graphics.setStrokeStyle(10);
+            graphics.drawCircle(0, 0, 30);
+            yellowPiece.visible = false;
+            stage.addChild(yellowPiece);
+            markers.o.push(yellowPiece);
+
+            //Create the red token/game piece
+            let redPiece = new createjs.Shape();
+            graphics = redPiece.graphics;
+            graphics.beginStroke('#FF0000');
+            graphics.beginFill('#FF0000');
+            graphics.setStrokeStyle(10);
+            graphics.drawCircle(0, 0, 30);
+            redPiece.visible = false;
+            stage.addChild(redPiece);
+            markers.o.push(redPiece);
+        }
+
+        // store markers and stage in component state
+        this.set('markers', markers);
+        this.set('stage', stage);
+
+
+
+
+
         stage.update()
     }
 });
